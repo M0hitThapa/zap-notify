20a5974e65c8d95bd212f62f1a8cb3fec52a4414
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-03 20:11:47 +0545

    made a heading reusable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Heading } from "@/components/heading"
 import { MaxWidthWrapper } from "../components/max-width-wrapper"
 
 const Page = () => {
@@ -5,7 +6,11 @@ return <>
 <section className="relative py-24 sm:py-32 bg-brand-25">
   <MaxWidthWrapper className="text-center">
     <div className="relative mx-auto flex flex-col text-center items-center gap-10">
-
+<div>
+  <Heading>
+    Real Time Saas Insights,
+  </Heading>
+</div>
     </div>
   </MaxWidthWrapper>
 </section>
