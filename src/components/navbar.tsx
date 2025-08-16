f07718717feff1affe003d29ed0b9d023ac9af2c
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-16 18:41:01 +0545

    added shadcn setup with its button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link"
 import { MaxWidthWrapper } from "./max-width-wrapper"
+import { SignOutButton } from "@clerk/nextjs"
 
 export const Navbar = () => {
+    const user = false
     return <div className="sticky h-16 z-[100] top-0 bg-white/80 border-b border-gray-100">
         <MaxWidthWrapper>
             <div className="flex items-center justify-between h-16">
                 <Link href="/" className="flex z-40 font-bold text-3xl text-shadow-2xs bg-gradient-to-r from-black via-cyan-800 to-cyan-600 bg-clip-text text-transparent">ZapLink</Link>
+                <div className="h-full flex items-center space-x-4">
+                    {user ? <>
+                    <SignOutButton>
+                        <button>
+                            
+                        </button>
+                    </SignOutButton>
+                    </> : null}
+
+                </div>
             </div>
         </MaxWidthWrapper>
         </div>
