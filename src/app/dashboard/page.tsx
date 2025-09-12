040a993d8042ec785a3ab7caa61c07397edfd4dd
Mohit Thapa <thapamohit2058@gmail.com>
2025-09-12 23:13:40 +0545

    made separate query wrapper component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,36 @@
-'use client'
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import MainPage from "./maindashboard/page"
 
- const Page = () => {
 
-    const queryClient = new QueryClient()
+
+import { QueryClientProviderWrapper } from "@/components/query-client-provider-wrapper"
+import { DashboardPage } from "@/components/dashboard-page"
+import { DashboardContentPage } from "./maindashboard/dashboard-content-page"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import prisma from "@/lib/prisma"
+
+ const Page = async() => {
+
+    const auth = await currentUser()
+
+    if(!auth) {
+        redirect('/sign-in')
+    }
+
+    const user = await prisma.user.findUnique({
+        where:{
+            externalId:auth.id
+        }
+    })
+    if(!user) {
+        redirect('/welcome')
+    }
+
+
     return (
-        <QueryClientProvider client={queryClient}>
-            <div>
-        <MainPage />
-    </div>
-        </QueryClientProvider>
+        <QueryClientProviderWrapper>
+            <DashboardPage title="Dashboard"><DashboardContentPage /></DashboardPage>
+        </QueryClientProviderWrapper>
     )
 }
 
